8df9c1827e8f2260ae054579338ed22af16814ff
SebastianGB <sebasgarcia09@gmail.com>
2025-04-07 11:44:17 +0200

    Create JIRA Project from SF modal

--- force-app/main/default/lwc/jiraProjectWizard/jiraProjectWizard.js
+++ force-app/main/default/lwc/jiraProjectWizard/jiraProjectWizard.js
@@ -1,104 +1,111 @@
-import { api, LightningElement, wire } from 'lwc';
-import checkJiraKeyAvailable from '@salesforce/apex/JiraProjectWizardController.checkJiraKeyAvailable';
-import createProject from '@salesforce/apex/JiraProjectWizardController.createProject';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { LightningElement, api, wire, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { CloseActionScreenEvent } from "lightning/actions";
+import checkJiraKeyAvailable from "@salesforce/apex/JiraProjectWizardController.checkJiraKeyAvailable";
+import createProject from "@salesforce/apex/JiraProjectWizardController.createProject";
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 import PROJECT_KEY from "@salesforce/schema/Account.Jira_Project_Key__c";
 import PROJECT_NAME from "@salesforce/schema/Account.Jira_Project_Name__c";
-import PROJECT_ID from "@salesforce/schema/Account.Jira_Project_Key__c";
+import PROJECT_ID from "@salesforce/schema/Account.Jira_Project_Id__c";
 
 export default class JiraProjectWizard extends LightningElement {
+  filter = {
+    criteria: [
+      {
+        fieldPath: "Jira_User_Id__c",
+        operator: "ne",
+        value: ""
+      }
+    ]
+  };
 
-    @api recordId;
-    @api objectNameId;
-    projectLead;
-    projectName;
-    key;
-    available;
-    notCheckedKey = true;
-    projectCreationDisabled = true;
-
-    filter = {
-        criteria: [
-          {
-            fieldPath: "JIRA_User_Id__c",
-            operator: "ne",
-            value: ""
-          }
-        ]
-      };
-
-    @wire(getRecord, {
-        recordId:"$recordId",
-        fields: [PROJECT_KEY, PROJECT_NAME, PROJECT_ID]
-    })
-
-    wiredAccount({error, data}) {
-        if (data) {
-            this.account = data;
-            if (getFieldValue(data, PROJECT_ID)) {
-                this.dispatchEvent(new CloseActionScreenEvent());
-                this.dispatchEvent(new ShowToastEvent({
-                    title: 'Project already exists DUH',
-                    message: 'You are trying to create a project that already exists.',
-                    variant:'error'
-                }));
-            }
-        } else if (error) {
-            console.log(error);
-        }
-
+  @api recordId;
+  @api objectApiName;
+  @track account;
+  available;
+  notCheckedKey = true;
+  projectCreationDisable = true;
+  projectName;
+  projectLead;
+  key;
+  error;
+  
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [PROJECT_KEY, PROJECT_NAME, PROJECT_ID]
+  })
+  wiredAccount({ error, data }) {
+    if (data) {
+      this.account = data;
+      if (getFieldValue(data, PROJECT_ID)) {
+        this.dispatchEvent(new CloseActionScreenEvent());
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error",
+            message: "Jira Project Already Created",
+            variant: "error"
+          })
+        );
+      }
+    } else if (error) {
+      this.error = error;
     }
-    handleKeyChange(event) {
-        this.key = event.target.value;
-    }
+  }
 
-    handleNameChange(event) {
-        this.projectName = event.target.value;
-    }
+  handleKeyChange(event) {
+    this.key = event.target.value;
+  }
 
-    handleLeadChange(event) {
-        this.projectLead = event.target.value;
-        this.checkProjectCreation();
-    }
+  handleNameChange(event) {
+    this.projectName = event.target.value;
+    this.checkProjectCreation();
+  }
 
-    async handleKeyCheck() {
-        try {
-            this.available = await checkJiraKeyAvailable({ key: this.key });
-            this.notCheckedKey = false;
-            this.checkProjectCreation();
-        } catch (error) {
-            console.error(error);
-        }
-    }
+  handleLeadChange(event) {
+    this.projectLead = event.detail.recordId;
+    this.checkProjectCreation();
+  }
 
-    checkProjectCreation() {
-        this.projectCreationDisabled = !this.available || !this.projectLead || this.notCheckedKey;
-    }
+  checkProjectCreation() {
+    this.projectCreationDisable =
+      !this.available || this.projectLead === undefined || this.notCheckedKey;
+    console.log(this.projectCreationDisable);
+  }
 
-    async handleProjectCreate() {
-        try {
-            await createProject({
-                key: this.key,
-                projectName: this.projectName,
-                accId: this.recordId,
-                userId: this.projectLead
-            });
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Success',
-                    message: 'Jira Project Created',
-                    variant: 'success'
-                })
-            );
-        } catch (error) {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error',
-                    message: 'Could not create Jira Project',
-                    variant: 'error'
-                })
-            );
-        }
+  async handleKeyCheck() {
+    try {
+      this.available = await checkJiraKeyAvailable({ key: this.key });
+      this.notCheckedKey = false;
+      this.checkProjectCreation();
+    } catch (e) {
+      this.error = e;
     }
+  }
+
+  async handleProjectCreate() {
+    try {
+      await createProject({
+        key: this.key,
+        projectName: this.projectName,
+        accId: this.recordId,
+        userId: this.projectLead
+      });
+      this.dispatchEvent(new CloseActionScreenEvent());
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Success",
+          message: "Jira Project Created",
+          variant: "success"
+        })
+      );
+    } catch (e) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Error",
+          message: "Could not create Jira project",
+          variant: "error"
+        })
+      );
+    }
+  }
 }
